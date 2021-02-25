cb09badc381960615e2c4b5c7784214251592664
Thiago Moraes <thiagodmor@gmail.com>
2021-02-25 10:35:25 -0300

    feat: add parameter validation

--- __tests__/calculator.test.js
+++ __tests__/calculator.test.js
@@ -1,39 +1,83 @@
-test('should return a missing parameter error [investmentDate]', () => {
+const Calculator = require('../src/lib/calculator');
+const { expect } = require('@jest/globals');
 
+const prices = {
+    '2021-01-01': 19.23,
+    '2021-01-02': 21.30,
+    '2021-01-03': 20.64,
+};
+
+test('should return a missing parameter error [investmentDate]', () => {
+    const calculator = new Calculator(100, null, '2020-01-01', prices);
+    const result = calculator.validateParams();
+
+    expect(result.valid).toBe(false);
+    expect(result.message).toBe('missing parameter [investmentDate]');
 });
 
 test('should return a missing parameter error [cdbRate]', () => {
+    const calculator = new Calculator(null, '2020-01-01', '2020-01-01', prices);
+    const result = calculator.validateParams();
 
+    expect(result.valid).toBe(false);
+    expect(result.message).toBe('missing parameter [cdbRate]');
 });
 
 test('should return a missing parameter error [currentDate]', () => {
+    const calculator = new Calculator(100, '2020-01-01', null, prices);
+    const result = calculator.validateParams();
 
-});
-
-test('should return a invalid date format error [currentDate]', () => {
-
+    expect(result.valid).toBe(false);
+    expect(result.message).toBe('missing parameter [currentDate]');
 });
 
 test('should return a invalid date format error [investmentDate]', () => {
+    const calculator = new Calculator(100, '20-01-0111', '2020-01-01', prices);
+    const result = calculator.validateParams();
 
+    expect(result.valid).toBe(false);
+    expect(result.message).toBe('invalid date format [investmentDate]');
+});
+
+test('should return a invalid date format error [currentDate]', () => {
+    const calculator = new Calculator(100, '2020-01-01', '201-1-1', prices);
+    const result = calculator.validateParams();
+
+    expect(result.valid).toBe(false);
+    expect(result.message).toBe('invalid date format [currentDate]');
 });
 
 test('should return a invalid value type [cdbRate]', () => {
+    const calculator = new Calculator('100', '2020-01-01', '2020-01-01', prices);
+    const result = calculator.validateParams();
 
+    expect(result.valid).toBe(false);
+    expect(result.message).toBe('invalid cdbRate value');
+});
+
+test('should return a invalid date error [currentDate < investmentDate]', () => {
+    const calculator = new Calculator(100, '2020-01-01', '2019-01-01', prices);
+    const result = calculator.validateParams();
+
+    expect(result.valid).toBe(false);
+    expect(result.message).toBe('invalid date error [currentDate must be greater than invesmentDate]');
 });
 
 test('should return valid parameters', () => {
+    const calculator = new Calculator(100, '2020-01-01', '2020-01-01', prices);
+    const result = calculator.validateParams();
 
+    expect(result.valid).toBe(true);
 });
 
-test('should return a valid TCDI tax value', () => {
+// test('should return a valid TCDI tax value', () => {
 
-});
+// });
 
-test('should return a CDI tax value', () => {
+// test('should return a CDI tax value', () => {
 
-});
+// });
 
-test('should return a list of CDBs between investmentDate and currentDate', () => {
+// test('should return a list of CDBs between investmentDate and currentDate', () => {
 
-});
+// });
--- src/lib/calculator.js
+++ src/lib/calculator.js
@@ -1,3 +1,5 @@
+const moment = require('moment-timezone');
+
 class Calculator {
     constructor(cdbRate, investmentDate, currentDate, cdiPrices) {
         this.cdbRate = cdbRate;
@@ -7,7 +9,63 @@ class Calculator {
     }
 
     validateParams() {
+        if (!this.cdbRate) {
+            return {
+                valid: false,
+                message: 'missing parameter [cdbRate]'
+            };
+        }
 
+        if (!this.investmentDate) {
+            return {
+                valid: false,
+                message: 'missing parameter [investmentDate]'
+            };
+        }
+
+        if (!this.currentDate) {
+            return {
+                valid: false,
+                message: 'missing parameter [currentDate]'
+            };
+        }
+
+        if (!this.validateDateFormat(this.investmentDate)) {
+            return {
+                valid: false,
+                message: 'invalid date format [investmentDate]'
+            };
+        }
+
+        if (!this.validateDateFormat(this.currentDate)) {
+            return {
+                valid: false,
+                message: 'invalid date format [currentDate]'
+            };
+        }
+
+        if (typeof this.cdbRate !== 'number') {
+            return {
+                valid: false,
+                message: 'invalid cdbRate value'
+            };
+        }
+
+        if (+new Date(this.currentDate) < +new Date(this.investmentDate)) {
+            return {
+                valid: false,
+                message: 'invalid date error [currentDate must be greater than invesmentDate]'
+            };
+        }
+
+        return {
+            valid: true,
+            message: null
+        };
+    }
+
+    validateDateFormat(date) {
+        return moment(date, 'YYYY-MM-DD', true).isValid();
     }
 
     getCDIListBetweenTwoValues() {
